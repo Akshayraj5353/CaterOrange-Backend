b0ca54bc745f791944580ae1b0a8bbbc4a041d72
juhi <juhi@scaleorange.com>
2024-04-08 11:10:35 +0530

    first commit

---
+++ routes/Userroutes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const validateToken = require('../middileware/validateToken');
+const {loginData, token} = require('../controllers/LoginController');
+const {signup} = require('../controllers/SignupController')
+const {CreateOrderDetails, getAllOrderDetails, getOrderDetailsById, updateOrderDetails, deleteOrderDetails} = require('../controllers/MyOrderController')
+
+
+router.post("/login", loginData);
+
+router.post("/signup", signup)
+
+router.post("/CreateOrderDetails", CreateOrderDetails);
+
+router.get('/getAllOrderDetails', getAllOrderDetails);
+
+router.get('/getOrderDetailsById', getOrderDetailsById);
+
+router.put('/updateOrderDetails', updateOrderDetails);
+
+router.delete('deleteOrderDetails', deleteOrderDetails);
+
+module.exports = router;
